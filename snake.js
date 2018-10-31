49fd15f1e316f9d77179dcb5447f148da3bf585e
Ismael P. Paiva <ismael.paiva@fatec.sp.gov.br>
2018-10-31 11:39:24 -0300

    almost done

--- snake.js
+++ snake.js
@@ -1,12 +1,11 @@
+// window.onload = function () {
 const CVS = document.getElementById('canvas');
 const CTX = CVS.getContext('2d');
 
 // unit (tamanho do BOX)
-
 const BOX = 32;
 
 // GROUND and apple img loading
-
 const GROUND = new Image();
 GROUND.src = 'img/GROUND.png';
 
@@ -14,43 +13,26 @@ const APPLEIMG = new Image();
 APPLEIMG.src = 'img/apple.png';
 
 // create the Random apple
-
 let apple = {
   x: Math.floor(Math.random() * 17 + 1) * BOX,
   y: Math.floor(Math.random() * 15 + 3) * BOX
 };
 
-// snake
-
-const snake = [];
-snake[0] = {
+// SNAKE
+let SNAKE = [];
+SNAKE[0] = {
   x: 9 * BOX,
   y: 10 * BOX
 };
 
-// directions of the snake
-
+// directions of the SNAKE
 let dir;
-
 document.addEventListener('keydown', direction);
-
-// create portals
-// const portal1 = [];
-// portal1[0] = {
-//   x: 1 * (BOX),
-//   y: 3 * (BOX)
-// };
-
-// const portal2 = [];
-// portal2[0] = {
-//   x: 17 * BOX,
-//   y: 18 * BOX - 96
-// };
 
 // score of the game
 let score = 0;
 
-let GameSection = 1;
+let gameSession = 1;
 
 // keep the score
 const PLAYER = {
@@ -59,18 +41,18 @@ const PLAYER = {
 };
 
 function takeScore() {
-  if (GameSection === 1) {
+  if (gameSession === 1) {
     PLAYER.playerA = score;
-    GameSection += 1;
-    console.log('blablabla');
+    gameSession += 1;
+    console.log(score);
   } else {
     PLAYER.playerB = score;
-    GameSection -= 1;
+    gameSession -= 1;
   }
-}console.log(score);
+  console.log(PLAYER);
+}
 
-// function to get the directions of the snake
-
+// function to get the directions of the SNAKE
 function direction(event) {
   const btn = event.keyCode;
   if (btn === 37 && dir !== 'RIGHT') {
@@ -94,31 +76,31 @@ function collision(head, array) {
   return false;
 }
 
+function reset() {
+  SNAKE = [];
+  SNAKE[0] = {
+    x: 9 * BOX,
+    y: 10 * BOX
+  };
+  draw();
+  // takeScore();
+}
+
 // draw all in to the canvas
 function draw() {
   CTX.drawImage(GROUND, 0, 0);
 
-  for (let i = 0; i < snake.length; i += 1) {
+  for (let i = 0; i < SNAKE.length; i += 1) {
     CTX.fillStyle = i === 0 ? 'green' : 'white';
-    CTX.fillRect(snake[i].x, snake[i].y, BOX, BOX);
-
-    CTX.strokeStyle = 'red';
-    CTX.strokeRect(snake[i].x, snake[i].y, BOX, BOX);
+    CTX.fillRect(SNAKE[i].x, SNAKE[i].y, BOX, BOX);
   }
-
-  // portals
-  // CTX.fillStyle = 'black';
-  // CTX.fillRect(portal1[0].x, portal1[0].y, 32, 96);
-
-  // CTX.fillStyle = 'black';
-  // CTX.fillRect(portal2[0].x, portal2[0].y, 32, 96);
 
   // apple
   CTX.drawImage(APPLEIMG, apple.x, apple.y);
 
   // old head position
-  let snakeX = snake[0].x;
-  let snakeY = snake[0].y;
+  let snakeX = SNAKE[0].x;
+  let snakeY = SNAKE[0].y;
 
   // which direction
   if (dir === 'LEFT') {
@@ -134,7 +116,7 @@ function draw() {
     snakeY += BOX;
   }
 
-  // if the snake eats the food
+  // if the SNAKE eats the food
   if (snakeX === apple.x && snakeY === apple.y) {
     score += 1;
     apple = {
@@ -144,42 +126,37 @@ function draw() {
     // we don't remove the tail
   } else {
     // remove the tail
-    snake.pop();
+    SNAKE.pop();
   }
 
   // add new Head
-
   const NEWHEAD = {
     x: snakeX,
     y: snakeY
   };
 
-  // draw the portal
-
-  // function to move the snake across the limits of the board
-  // function MoveAcross() {
-  //   if (snakeX === portal1) {
-
-  //   }
-  // }
-
   // game over
   if (
     snakeX < BOX
-    || snakeX > 17 * BOX
-    || snakeY < 3 * BOX
-    || snakeY > 17 * BOX
-    || collision(NEWHEAD, snake)
+      || snakeX > 17 * BOX
+      || snakeY < 3 * BOX
+      || snakeY > 17 * BOX
+      || collision(NEWHEAD, SNAKE)
   ) {
     clearInterval(game);
-    takeScore(game);
+    setTimeout(reset, 1000);
+    takeScore();
   }
-  // if ((snakeX >= portal1.x && snakeX <= portal1.x + 32) && (snakeY >= portal1.y && snakeY <= portal1.y + 96)) {}
 
-  snake.unshift(NEWHEAD);
+  SNAKE.unshift(NEWHEAD);
 
   CTX.fillStyle = 'white';
   CTX.font = '45px Open sans';
   CTX.fillText(score, 2 * BOX, 1.6 * BOX);
 }
-let game = setInterval(draw, 150);
+let game;
+function startGame() {
+  score = 0;
+  game = setInterval(draw, 150);
+}
+// };
--- snake2.js
+++ snake2.js
@@ -2,22 +2,117 @@ window.onload = function () {
   const cvs = document.getElementById('canvas');
   const ctx = cvs.getContext('2d');
 
-  setInterval(game, 60);
+  const cvsW = cvs.width;
+  const cvsH = cvs.height;
 
-  const step = 1;
+  const snakeW = 20;
+  const snakeH = 20;
 
-  let x = 0;
-  let y = 0;
+  // score
+  let score = 0;
 
-  let px = 0;
-  let py = 15;
+  // defaut
+  let direction = 'right';
 
-
-  function game() {
-
+  // Event listener
+  document.addEventListener('keydown', getDirection);
+  function getDirection(e) {
+    if (e.keyCode === 37 && direction !== 'right') {
+      direction = 'left';
+    } else if (e.keyCode === 38 && direction !== 'down') {
+      direction = 'up';
+    } else if (e.keyCode === 39 && direction !== 'left') {
+      direction = 'right';
+    } else if (e.keyCode === 40 && direction !== 'up') {
+      direction = 'down';
+    }
   }
 
+  function drawSnake(x, y) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(x * snakeW, y * snakeH, snakeW, snakeH);
+  }
+  const len = 4;
+  const snake = [];
 
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, cvs.width, cvs.height);
+  // snake obj
+  for (let i = len - 1; i >= 0; i -= 1) {
+    snake.push({
+      x: 1,
+      y: 0
+    });
+  }
+
+  // create apple
+  let apple = {
+    x: Math.floor(Math.random() * (cvsW / snakeW - 1) + 1),
+    y: Math.floor(Math.random() * (cvsH / snakeH - 1) + 1)
+  };
+
+  // draw the apple function
+  function drawApple(x, y) {
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(x * snakeW, y * snakeH, snakeW, snakeH);
+  }
+
+  // draw score
+  function drawScore() {
+    ctx.fillStyle = 'yellow';
+    ctx.filText('Score : ', score, 5, cvsH - 5); // ?????
+  }
+
+  // check collision function
+  function collision(head, array) {
+    for (let i = 0; i < array.length; i += 1) {
+      if (head.x === array[i].x && head.y === array[i].y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // DRAW everthing
+  function draw() {
+    ctx.clearRect(0, 0, cvsW, cvsH);
+    for (let i = 0; i < snake.length; i += 1) {
+      const x = snake[i].x;
+      const y = snake[i].y;
+      drawSnake(x, y);
+    }
+
+    // call function draw apple
+    drawApple(apple.x, apple.y);
+    // snake head
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // direction
+    if (direction === 'left') snakeX -= 1;
+    else if (direction === 'up') snakeY -= 1;
+    else if (direction === 'right') snakeX += 1;
+    else if (direction === 'down') snakeY += 1;
+
+    // increment snake
+    if (snakeX === apple.x && snakeY === apple.y) {
+      score += 1;
+      apple = {
+        x: Math.floor(Math.random() * (cvsW / snakeW - 1) + 1),
+        y: Math.floor(Math.random() * (cvsH / snakeH - 1) + 1)
+      };
+    } else {
+      snake.pop();
+    }
+    const newHead = {
+      x: snakeX,
+      y: snakeY
+    };
+    snake.unshift(newHead);
+
+    // se Collision
+    // if ((snakeX < 0 || snakeY < 0 || snakeX >= cvsW || snakeY >= cvsH|| collision(snakeX, snakeY, snake)))
+  }
+  CTX.fillStyle = 'white';
+  CTX.font = '45px Open sans';
+  CTX.fillText(score, 2 * , 1.6 * BOX);
+  const game = setInterval(draw, 150);
 };
